49cf733b0d8a78ecb68b82e1cff14f6a469314db
talel <talelmejri8@gmail.com>
2023-08-05 12:44:49 +0100

    add authCompoentn(login,signUp)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './Auth/login-component/login-component.component';
+import { SignUpComponentComponent } from './Auth/sign-up-component/sign-up-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    SignUpComponentComponent
   ],
   imports: [
     BrowserModule
