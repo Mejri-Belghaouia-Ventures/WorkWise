11c60cf3da3a504b5827a731d966dc6261cb9d91
talel <talelmejri8@gmail.com>
2023-08-06 11:01:08 +0100

    fix app_Routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes ,RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './Auth/login-component/login-component.component';
+import { SignUpComponentComponent } from './Auth/sign-up-component/sign-up-component.component';
 
-
-
+const routes: Routes = [
+  {path:"",component:AppComponent},
+  {path:"login",component:LoginComponentComponent},
+  {path:"signup",component:SignUpComponentComponent},
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
